3c274d1bea9221a98a525ad3cc425ccef9931943
42tg <tobiasgraf87@gmail.com>
2020-02-02 20:03:01 +0100

    Base logic implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,99 @@
-import React from "react";
-import { Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  ThemeProvider,
+  createMuiTheme
+} from "@material-ui/core";
 import "./App.css";
+import { produce } from "immer";
 
-import Base from "./examples/Base.txt";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Base from "!raw-loader!./examples/Base.txt";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Subtract from "!raw-loader!./examples/Subtract.txt";
+
+const CONSTANT_ROWS = 20;
+
+const toObject = produce((txtFile: string) =>
+  txtFile
+    .split("\n")
+    .map((line: string) => line.split(" "))
+    .map(([count, ...name]: string[]) => [count, name.join(" ")])
+    .filter((line: string[]) => parseInt(line[0], 10))
+    .reduce(
+      (prev: {}, [count, name]: string[]) => ({
+        ...prev,
+        [name]: parseInt(count, 10)
+      }),
+      {}
+    )
+);
 
 const App: React.FC = () => {
+  const [base, setBase] = useState(Base);
+  const [subtract, setSubstract] = useState(Subtract);
+  const [result, setResult] = useState({});
+
+  const getDiff = () => {
+    const baseObj = toObject(base);
+    const subtractObj = toObject(subtract);
+
+    setResult(
+      produce(() => {
+        const obj = Object.entries(baseObj).reduce(
+          (prev, [key, value]: [string, number]) => ({
+            ...prev,
+            [key]: value
+          }),
+          {}
+        );
+        Object.entries(subtractObj).forEach(
+          ([key, value]: [string, number]) => {
+            if (obj[key]) {
+              obj[key] = obj[key] - value;
+            }
+          }
+        );
+        return obj;
+      })
+    );
+  };
   return (
-    <div className="App">
-      <div className="App-content App">
-        <TextField
-          label="Base"
-          multiline
-          rows="10"
-          defaultValue=""
-          variant="outlined"
-        ></TextField>
-        <Button>Sutract</Button>
-        <TextField
-          label="Subtract"
-          multiline
-          rows="10"
-          defaultValue=""
-          variant="outlined"
-        ></TextField>
+    <ThemeProvider theme={createMuiTheme()}>
+      <div>
+        <div className="App">
+          <TextField
+            label="To Buy"
+            multiline
+            rows={CONSTANT_ROWS}
+            defaultValue={base}
+            onChange={e => setBase(produce(() => e.target.value))}
+            variant="outlined"
+          ></TextField>
+          <Button color="primary" onClick={getDiff}>
+            Get Buylist
+          </Button>
+          <TextField
+            label="Already Own"
+            multiline
+            rows={CONSTANT_ROWS}
+            onChange={e => setSubstract(produce(() => e.target.value))}
+            defaultValue={subtract}
+            variant="outlined"
+          ></TextField>
+        </div>
+        {Object.entries(result).length > 0 && (
+          <div className="App column">
+            {Object.entries(result).map(([key, value]) => (
+              <div key={key} className={value > 0 ? "green" : "red"}>
+                {value} {key}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
